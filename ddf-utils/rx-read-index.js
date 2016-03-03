e3dbd13234e297d1a6c91d28f95091496ec7ab94
buchslava <vyacheslav.chub@gmail.com>
2016-03-03 17:46:25 +0200

    feat(bootstrap): start new ddf functionality
    also provided ddf root folder validation
    
    Closes #10
    Closes #9

--- ddf-utils/rx-read-index.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const ddfIndexSchema = require('../ddf-schema/ddf-index.schema');
-const rxReadDdfFile = require('../utils/rx-read-ddf-csv');
-module.exports = folderPath => rxReadDdfFile(folderPath, ddfIndexSchema.fileName);
--- index.js
+++ index.js
@@ -1,78 +1,12 @@
 #! /usr/bin/env node
 'use strict';
 
-// from args
-var userArgs = process.argv.slice(2);
-let folder = userArgs[0] || process.cwd();
+const utils = require('./lib/utils');
+const DDFRoot = require('./lib/files/root');
+const ddfRoot = new DDFRoot(utils.ddfRootFolder);
+ddfRoot.check(() => {
 
-const fs = require('fs');
-const path = require('path');
-const rx = require('rxjs');
-require('console.table');
-
-const errorCodes = require('./lib/ddf-error-codes');
-const logger = require('./utils/logger');
-
-const normalizedPath = require('./utils/path-normilize')(folder);
-
-if (!require('./utils/path-exists-sync')(normalizedPath)) {
-  logger.log(errorCodes.err_folder_not_found.message(folder));
-  return;
-}
-
-const files$ = require('./utils/rx-recursive-readdir')(normalizedPath);
-
-const folders$ = files$
-  .filter(file => fs.lstatSync(file).isDirectory());
-
-const ddfFolders$ = folders$
-  .filter(folder => require('./utils/path-is-ddf-folder-sync')(folder));
-
-// validate list of ddf folders
-ddfFolders$.count()
-  .combineLatest([ddfFolders$.toArray(), folders$.count()],
-    (ddfFoldersCount, ddfFolders, foldersCount)=> {
-      return {ddfFolders, ddfFoldersCount, foldersCount};
-    })
-  .subscribe(res => {
-    const isFolderValid = require('./lib/ddf-root-folder.validator.js')
-    (folder, res.ddfFoldersCount, res.foldersCount);
-
-    if (!isFolderValid) {
-      return;
-    }
-
-    const plural = res.ddfFoldersCount === 1 ? '' : 's';
-    logger.log(`Found ${res.ddfFoldersCount} DDF folder${plural}, processing...:`);
-    logger.log(res.ddfFolders);
-
-    return validateDdfFolder(ddfFolders$);
-  }, err => console.error(err));
-
-// validate each ddfFolder
-function validateDdfFolder(ddfFolders$) {
-  ddfFolders$
-    .do(x=>console.log('Validating ddf folder', x))
-    .map(folderPath => {
-      // validate dimensions file
-      const dimensionsFile$ = require('./ddf-utils/rx-read-dimension')(folderPath);
-      const isDimensionsValid = require('./lib/ddf-dimensions.validator')(folderPath, dimensionsFile$);
-
-      // validate measures file
-      const measuresFile$ = require('./ddf-utils/rx-read-measures')(folderPath);
-      const isMeasuresValid = require('./lib/ddf-measures.validator')(folderPath, measuresFile$);
-
-      // validate dimensions&measures unique ids
-      const isIdUnique = require('./lib/ddf-dimensions-and-measures-unique-id.validator.js')
-      (folderPath, dimensionsFile$, measuresFile$);
-
-      return isDimensionsValid
-        .combineLatest([isMeasuresValid, isIdUnique], (a, b, c)=> {
-          const d = a.concat(b).concat(c);
-          console.table(d);
-          return d;
-        });
-    })
-    //.do(x=>console.log(x))
-    .subscribe(x=>x.subscribe(), x=>console.error(x.stack));
-}
+  ddfRoot.getDdfDirectoriesDescriptors().forEach(d => {
+    console.log(`${d.dir} ${d.isEmpty} ${d.isDDF}`);
+  });
+});
