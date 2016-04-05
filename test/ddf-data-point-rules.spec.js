bd1c9169030e1a39f5583f8109df4463e80926fe
buchslava <vyacheslav.chub@gmail.com>
2016-04-05 10:48:02 +0300

    feat(data points): data points validation
    Closes #34

--- test/ddf-data-point-rules.spec.js
+++ test/ddf-data-point-rules.spec.js
@@ -1,4 +1,5 @@
 'use strict';
+const _ = require('lodash');
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
 const DdfData = require('../lib/ddf-definitions/ddf-data');
@@ -45,14 +46,52 @@ describe('rules for data points', () => {
 
         ddfData.getDataPoint().loadDetail(expectedDataPointDetail, () => {
           const issues = dataPointValueNotNumRule(ddfData, expectedDataPointDetail);
+          const issue = _.head(issues);
 
           expect(issues.length).to.equal(1);
-          expect(issues[0].type).to.equal(rulesRegistry.DATA_POINT_VALUE_NOT_NUMERIC);
-          expect(issues[0].path.endsWith(expectedFileName)).to.be.true;
-          expect(!!issues[0].data).to.be.true;
-          expect(issues[0].data.measure).to.equal(expectedMeasure);
-          expect(issues[0].data.line).to.equal(expectedLine);
-          expect(issues[0].data.value).to.equal(expectedValue);
+          expect(issue.type).to.equal(rulesRegistry.DATA_POINT_VALUE_NOT_NUMERIC);
+          expect(issue.path.endsWith(expectedFileName)).to.be.true;
+          expect(!!issue.data).to.be.true;
+          expect(issue.data.measure).to.equal(expectedMeasure);
+          expect(issue.data.line).to.equal(expectedLine);
+          expect(issue.data.value).to.equal(expectedValue);
+
+          done();
+        });
+      });
+    });
+  });
+
+  describe('when "DATA_POINT_UNEXPECTED_ENTITY_VALUE" rule', () => {
+    afterEach(done => {
+      ddfData.dismiss(() => {
+        done();
+      });
+    });
+
+    it(`any issue should NOT be found for folder without the problem
+    (fixtures/rules-cases/data-point-unexpected-entity-value)`, done => {
+      ddfData = new DdfData('./test/fixtures/rules-cases/data-point-unexpected-entity-value');
+      ddfData.load(() => {
+        const dataPointUnexpectedConceptRule =
+          dataPointsRules[rulesRegistry.DATA_POINT_UNEXPECTED_ENTITY_VALUE];
+        const expectedDataPointDetail = ddfData.getDataPoint().details[0];
+        const expectedFileName = 'ddf--datapoints--pop--by--country--year.csv';
+        const expectedConcept = 'country';
+        const expectedLine = 2;
+        const expectedValue = 'non-usa';
+
+        ddfData.getDataPoint().loadDetail(expectedDataPointDetail, () => {
+          const issues = dataPointUnexpectedConceptRule(ddfData, expectedDataPointDetail);
+          const issue = _.head(issues);
+
+          expect(issues.length).to.equal(1);
+          expect(issue.type).to.equal(rulesRegistry.DATA_POINT_UNEXPECTED_ENTITY_VALUE);
+          expect(issue.path.endsWith(expectedFileName)).to.be.true;
+          expect(!!issue.data).to.be.true;
+          expect(issue.data.concept).to.equal(expectedConcept);
+          expect(issue.data.line).to.equal(expectedLine);
+          expect(issue.data.value).to.equal(expectedValue);
 
           done();
         });
