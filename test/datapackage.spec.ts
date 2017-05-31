d1b98c6cdd4dcc95939ea0c968c27301d4ae31a2
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-05-31 14:46:06 +0300

    chore(datapackage): apply command line settings on datapackage creation functionality
    Closes #406

--- test/datapackage.spec.ts
+++ test/datapackage.spec.ts
@@ -8,7 +8,7 @@ describe('datapackage validation', () => {
   describe('when "ddf--unpop--wpp_population" dataset', () => {
     it('should datapackage be created propery', done => {
       const dataPackageTemplate = require('./fixtures/data-package/ddf--unpop--wpp_population/datapackage.template.json');
-      const dataPackage = new DataPackage('./test/fixtures/data-package/ddf--unpop--wpp_population/');
+      const dataPackage = new DataPackage('./test/fixtures/data-package/ddf--unpop--wpp_population/', {});
 
       dataPackage.take(dataPackageObject => {
         expect(isEqual(dataPackageObject, dataPackageTemplate)).to.be.true;
@@ -19,7 +19,7 @@ describe('datapackage validation', () => {
   });
   describe('when "ddf--bubbles-3" dataset', () => {
     it('should datapackage be created propery with non strict ddf file names', done => {
-      const dataPackage = new DataPackage('./test/fixtures/ddf--bubbles-3/');
+      const dataPackage = new DataPackage('./test/fixtures/ddf--bubbles-3/', {});
 
       dataPackage.take(dataPackageObject => {
         const EXPECTED_RESOURCES_LENGTH = 3;
