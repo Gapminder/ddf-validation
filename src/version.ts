88ef4f2d48bb921ef2ec758b6d880d817ee3f950
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2017-05-31 18:10:58 +0300

    feat(general): message about non actual version
    command line -v parameter was added
    
    Closes #380

---
+++ src/version.ts
@@ -0,0 +1,16 @@
+import { exec } from 'child_process';
+import { isEmpty } from 'lodash';
+import { valid, lt } from 'semver';
+import { red, green } from 'chalk';
+
+export const checkLatestVersion = (currentVersion: string) => {
+  exec('npm show ddf-validation version', (err: any, _latestVersion: string) => {
+    const latestVersion = (_latestVersion || '').trim();
+
+    if (!err && !isEmpty(latestVersion)) {
+      if (valid(latestVersion) && lt(currentVersion, latestVersion)) {
+        console.log(`Current version ${red(currentVersion)} is too old. New version is ${green(latestVersion)}. Update please!`);
+      }
+    }
+  });
+};
