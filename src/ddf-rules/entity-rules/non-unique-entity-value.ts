b4b60215216e3bb369bd2c6ff9f5fc35bc9c2c50
semio <semio@posteo.net>
2022-10-16 11:36:18 +0800

    remove ts sources

--- src/ddf-rules/entity-rules/non-unique-entity-value.ts
+++
@@ -1,7 +0,0 @@
-import { DdfDataSet } from '../../ddf-definitions/ddf-data-set';
-import { SearchUniqueEntitiesStory } from './stories/duplicated-entities';
-
-export const rule = {
-  rule: (ddfDataSet: DdfDataSet) => new SearchUniqueEntitiesStory(ddfDataSet).fillServiceData()
-    .collectFieldDescriptors().collectValueCounters().collectValueDuplicationDescriptors().countDuplicationsByEntitySets().result()
-};
--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-import { getSettings, getDDFRootFolder } from './args';
-import { getLogger } from './logger';
-
-export { getTransport } from './logger';
-
-export const settings: any = getSettings();
-export const logger: any = getLogger();
-export const ddfRootFolder = getDDFRootFolder();
